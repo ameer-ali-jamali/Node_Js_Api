8785a7672d965705152441554e7003b76e0aff37
Ameer Ali Jamali <ameer.ali.jamali886@gmail.com>
2022-08-29 10:40:06 +0500

    90% post api working

--- backend/db_connect.js
+++ backend/db_connect.js
@@ -1,14 +1,49 @@
 const mongoose = require("mongoose");
 const validator = require('validator');
-mongoose.connect("mongodb://localhost:27017/local-data")
+mongoose.connect("mongodb://localhost:27017/std")
     .then(() => console.log("Connected Successfully....."))
     .catch((err) => console.log(err));
 
 
 const studentsSchema = new mongoose.Schema({
-    name: String,
-    Brand: String,
-    Price: String,
-    Categroy: String
+    First_name: {
+        type: String,
+        require: true,
+        minlength: 1,
+        unique: true
+    },
+    Last_name: {
+        type: String,
+        require: true,
+        minlength: 4
+    },
+    Email: {
+        type: String,
+        require: true,
+        minlength: 4,
+        unique: true
+    },
+    Password: {
+        type: String,
+        require: true,
+        minlength: 8,
+        unique: true
+    },
+    Phone_Number: {
+        type: Number,
+        min: 1,
+        require: true,
+        minlength: 8,
+        unique: true
+    },
+    Adress: {
+        type: String,
+        require: true,
+        minlength: 8,
+
+    }
+
 });
 
+const students = new mongoose.model('Students', studentsSchema);
+module.exports = students;
--- backend/post_api.js
+++ backend/post_api.js
@@ -1,15 +1,57 @@
-const express = require('express')
-require('./db_connect');
-const app = express();
+const express = require('express');
+const mongoose = require("mongoose");
+const validator = require('validator');
+mongoose.connect("mongodb://localhost:27017/Students_Data")
+    .then(() => console.log("Connected Successfully....."))
+    .catch((err) => console.log(err));
+///
 const port = 3000;
+const app = express();
+app.use(express.json());
 
-app.get("/students", (req, resp) => {
-    resp.send("hello");
+const User_dataSchema = new mongoose.Schema({
+    name: String,
+    password: String
+    // First_name: {
+    //     type: String,
+    //     require: true,
+    //     unique: true
+    // },
+    // Last_name: {
+    //     type: String,
+    //     require: true,
+
+    // },
+    // Email: {
+    //     type: String,
+    //     require: true,
+    //     unique: true
+    // },
+    // Password: {
+    //     type: String,
+    //     require: true,
+    //     unique: true
+    // },
+    // Address: {
+    //     type: String,
+    //     require: true
+
+    // }
+
 });
 
+const Students = mongoose.model('Aptech_Srudents', User_dataSchema);
 
-
-
+app.post("/user", async (req, resp) => {
+    const result = await new Students(req.body);
+    result.save()
+        .then(() => {
+            resp.send({ Result: "Data Submited" })
+        }).catch((err) => {
+            console.log(err)
+        });
+    console.log(result);
+});
 
 
 app.listen(port, () => {
