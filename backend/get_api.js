d6f35f6762dd7cf8c13b80bad2a7670d6729f49b
Ameer Ali Jamali <ameer.ali.jamali886@gmail.com>
2022-08-29 15:57:01 +0500

    get api done

--- backend/get_api.js
+++ backend/get_api.js
@@ -29,25 +29,24 @@ minutes = date_ob.getMinutes();
 seconds = date_ob.getSeconds();
 
 const user_input_data_model = mongoose.model('Aptech_Students', User_dataSchema);
-app.post("/user", async (req, resp) => {
-    const result = await new user_input_data_model(req.body);
-    result.save()
-        .then(() => {
-            resp.send(` Result : Your Data Submited Successfully
-           Currunt Date : ${date}/${month}/${year} ${hours}:${minutes}:${seconds}
-            ${result}
-           `);
-        }).catch((err) => {
-            console.log(err)
-            resp.send(`There is some error please solve error and try again
-            ${date}/${month}/${year} ${hours}:${minutes}:${seconds} 
-             ${err}`)
 
-        })
-    console.log(`${result} 
-    ${date}/${month}/${year} ${hours}:${minutes}:${seconds}`);
-});
-
+app.get("/get/:id", async (req, resp) => {
+    try {
+        const _id = req.params.id;
+        const result = await user_input_data_model.findById(_id);
+        resp.send(` Result : Your Data Submited Successfully
+        Currunt Date : ${date}/${month}/${year} ${hours}:${minutes}:${seconds}
+         ${result}
+        `)
+        console.log(`Your Data Successfully get from Database....
+        ${result}`)
+    } catch (err) {
+        console.log(err)
+        resp.send(`There is some error please solve error and try again
+        ${date}/${month}/${year} ${hours}:${minutes}:${seconds} 
+         ${err}`)
+    }
+})
 
 app.listen(port, () => {
     console.log(`Local Server Working On  http://localhost:${port}`)
--- backend/put_api.js
+++ backend/put_api.js
@@ -17,19 +17,30 @@ const User_dataSchema = new mongoose.Schema({
     password: String,
     phone: Number,
     address: String
-
 });
 
 const date_ob = new Date();
+
+// Current Date and time
+////////////////////////
+// adjust 0 before single digit date
 date = ("0" + date_ob.getDate()).slice(-2);
+// current month
 month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+// current year
 year = date_ob.getFullYear();
+// current hours
 hours = date_ob.getHours();
+// current minutes
 minutes = date_ob.getMinutes();
+// current seconds
 seconds = date_ob.getSeconds();
+////////////////////////
+
 
 const user_input_data_model = mongoose.model('Aptech_Students', User_dataSchema);
-app.post("/user", async (req, resp) => {
+
+app.get("/user", async (req, resp) => {
     const result = await new user_input_data_model(req.body);
     result.save()
         .then(() => {
